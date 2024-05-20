c17e79b2f41cf86ab9046cc273126d78e9729618
unknown <pirje.cristina@gmail.com>
2024-05-20 11:28:52 +0300

    curs21 final

--- src/main/java/tests/ActionClassExample.java
+++ src/main/java/tests/ActionClassExample.java
@@ -1,7 +1,13 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import pages.ContactsPage;
 import pages.MenuPage;
 import pages.ShopPage;
 import utils.BaseTest;
@@ -20,13 +26,38 @@ public class ActionClassExample extends BaseTest {
 	
 		
 	}
-	@Test
+	//@Test
 	public void dragAndDropExample() {
 		MenuPage menu= new MenuPage(driver);
 		menu.navigateTo(menu.shopLink);
 		ShopPage shop = new  ShopPage(driver);
 		shop.dragAndDrop(shop.sliderInitialPosition,100,0);
 		shop.dragAndDrop(shop.sliderFinalPosition, -100, 0);
+	
+	}
+	@Test
+	public void sendKeysExample() {
+		MenuPage menu= new MenuPage(driver);	
+		menu.navigateTo(menu.contactsLink);
+		
+		ContactsPage contacts = new ContactsPage(driver);
+		WebElement nameElement = driver.findElement(contacts.name);
+		Actions action = new Actions(driver);
+		
+		action
+		.moveToElement(nameElement)
+		.click(nameElement)
+		.sendKeys("nume")
+		.sendKeys(Keys.TAB,"email@email.com")
+		.pause(Duration.ofSeconds(2))
+		.sendKeys(Keys.TAB,"My subject")
+		.pause(Duration.ofSeconds(2))
+		.sendKeys(Keys.TAB,"My message")
+		.pause(Duration.ofSeconds(2))
+		.sendKeys(Keys.TAB,Keys.ENTER)
+		.pause(Duration.ofSeconds(2))
+		.perform();
+		
 		
 		
 		
